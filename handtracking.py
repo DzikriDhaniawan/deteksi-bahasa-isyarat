6e2094d9b5b019a2d5fe64c16ad7dcda9467d107
Dzikri Dhaniawan <dhaniawan.dzikri@gmail.com>
2024-12-08 19:20:24 +0800

    menambah bounding box

--- handtracking.py
+++ handtracking.py
@@ -11,7 +11,6 @@ def close_window():
     cv2.destroyAllWindows()
     exit()
 
-
 cap = cv2.VideoCapture(0)
 hands = mp_hands.Hands()
 
@@ -34,10 +33,19 @@ while True:
 
             landmark_array = np.array([[lm.x, lm.y, lm.z] for lm in hand_landmarks.landmark])
             landmark_array = landmark_array.flatten()
-
             landmark_array = (landmark_array - np.mean(landmark_array)) / np.std(landmark_array)
+
+            h, w, c = image_bgr.shape
+            landmark_coords = np.array([[int(lm.x * w), int(lm.y * h)] for lm in hand_landmarks.landmark])
+
+            x_min, y_min = np.min(landmark_coords, axis=0)
+            x_max, y_max = np.max(landmark_coords, axis=0)
+
+            cv2.rectangle(image_bgr, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
 
     cv2.imshow("Hand Gesture Recognition", image_bgr)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         close_window()
+
+cv2.destroyAllWindows()
